490fb10d5a7179035cf44ace25fb111c06752ca7
Tiange-Lei <encore0722@gmail.com>
2020-08-29 00:02:10 +1000

    remove gitignore files

--- aip/src/pages/userPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import NewFavourForm from '../components/newFavourForm';
-
-const UserPage = ()=>(
-    <div>
-        <NewFavourForm />
-    </div>
-)
-export default UserPage;
