cc3012f300230f9b324f3885d10ea485594d916f
fischkoder <vielenfisch212@gmail.com>
2020-10-08 12:07:46 +0800

    Create UserInfo and favourAwards schemas, create apis for searching users

--- aip-backend/src/routes.js
+++ aip-backend/src/routes.js
@@ -4,8 +4,6 @@ const passport = require('passport');
 const User = require('./userSchema');
 const Favour = require('./favourSchema');
 const bcrypt = require('bcryptjs');
-const friends = require('./friendsStatus');
-//const party = require('./party');
 const userInfo = require('./userInfo');
 
 var dataTransfer = [];
@@ -44,10 +42,10 @@ router.get("/logout",(req,res)=>{
   req.logOut();
   res.send('logout')
 })
-//User Operation
+//User Operation--------------------------------------------------------------
 //Load all users
 router.get('/api/loadUsers',(req,res)=>{
-  User.find({},(err,User)=>{
+  userInfo.find({},(err,User)=>{
     if(err) throw err;
     res.status(200).json(User);
   })
@@ -55,106 +53,12 @@ router.get('/api/loadUsers',(req,res)=>{
 //Search User
 router.get('/api/searchUsers/:name',(req,res)=>{
   console.log(req);
-  User.find({username:req.params.name},(err,User)=>{
+  userInfo.find({username:req.params.name},(err,User)=>{
     if(err) throw err;
     res.status(200).json(User);
   })
 })
-//Request for adding friends
-router.post('/api/requestFriend/:userA/:userB',async (req,res)=>{
-  const docA = await friends.findOneAndUpdate(
-      { requester: req.params.userA, recipient: req.params.userB },
-      { $set: { status: 1 }},
-      { upsert: true, new: true },(err,docA)=>{
-        if(err) throw err;
-        res.status(200).json(docA);
-      });
-  const docB = await friends.findOneAndUpdate(
-      { recipient: UserA, requester: UserB },
-      { $set: { status: 2 }},
-      { upsert: true, new: true },(err,docB)=>{
-        if(err) throw err;
-        res.status(200).json(docB);
-      });
-  const updateUserA = await userInfo.findOneAndUpdate(
-      { _id: req.params.userA },
-      { $push: { friends: docA._id }},(err,updateUserA)=>{
-        if(err) throw err;
-        res.status(200).json(updateUserA);
-      });
-  const updateUserB = await userInfo.findOneAndUpdate(
-      { _id: req.params.userB },
-      { $push: { friends: docB._id }},(err,updateUserB)=>{
-        if(err) throw err;
-        res.status(200).json(updateUserB);
-      });
-})
-//Accept friends
-router.post('/api/acceptFriend/:userA/:userB', (req,res)=>{
-  const docA = friends.findOneAndUpdate(
-      { requester: req.params.userA, recipient: req.params.userB },
-      { $set: { status: 3 }},(err,docA)=>{
-        if(err) throw err;
-        res.status(200).json(docA);
-      })
-  const docB = friends.findOneAndUpdate(
-      { recipient: req.params.userA, requester: req.params.userB },
-      { $set: { status: 3 }},(err,docB)=>{
-        if(err) throw err;
-        res.status(200).json(docB);
-      })
-})
-//Reject friends
-router.post('/api/rejectFriend/:userA/:userB',async (req,res)=>{
-  const docA = await friends.findOneAndRemove(
-      { requester: req.params.userA, recipient: req.params.userB },(err,docA)=>{
-        if(err) throw err;
-        res.status(200).json(docA);
-      });
-  const docB = await friends.findOneAndRemove(
-      { recipient: req.params.userA, requester: req.params.userB },(err,docB)=>{
-        if(err) throw err;
-        res.status(200).json(docB);
-      });
-  const updateUserA = await friends.findOneAndUpdate(
-      { _id: req.params.userA },
-      { $pull: { friends: docA._id }},(err,updateUserA)=>{
-        if(err) throw err;
-        res.status(200).json(updateUserA);
-      });
-  const updateUserB = await friends.findOneAndUpdate(
-      { _id: req.params.userB },
-      { $pull: { friends: docB._id }},(err,updateUserB)=>{
-        if(err) throw err;
-        res.status(200).json(updateUserB);
-      });
-})
 
-//Load Friend list
-router.get('/api/loadFriendlist/:currentuid',(req,res)=>{
-  userInfo.aggregate([
-              { "$lookup": {
-                "from": friends.collection.name,
-                "let": { "friends": "$friends" },
-                "pipeline": [
-                  { "$match": {
-                    "recipient": req.params.currentuid,
-                    "$expr": { "$in": [ "$_id", "$$friends" ] }
-                  }},
-                  { "$project": { "status": 1 } }
-                ],
-                "as": "friends"
-              }},
-              { "$addFields": {
-                "friendsStatus": {
-                  "$ifNull": [ { "$min": "$friends.status" }, 0 ]
-                }
-              }}
-            ]),(err,userInfo)=>{
-              if(err) throw err;
-              res.status(200).json(userInfo);
-            }
-});
 // ------------------------------------Favours Operation--------------------------------------------
 
 // loading favours---------------------------
@@ -226,30 +130,15 @@ router.post('/addcomment/:favourID',async (req,res)=>{
     })
   })
 })
-//api-friends list
 
-
-//party function
+//party detection
 router.get('/getParty', async (req,res,next) => {
     Favour.find({},{'publisher':1,'receiver':1,_id:0},(err,Favour)=>{
     if(err) throw err;
     res.status(200).json(Favour);
     dataTransfer = Favour;
     next();
-  })},()=>{
-      var list = [];
-      dataTransfer.forEach(doc => {
-      list.push(doc);
-      //party[0] = list[0];
-      console.log('a');
-      console.log(list);
-      });
-      const first = JSON.stringify({username:JSON.parse(list[0].publisher)});
-      var cache = [];
-      cache[0] = first;
-      
-
-  }
+  })}
 )
 
 //export routers------------------------------
