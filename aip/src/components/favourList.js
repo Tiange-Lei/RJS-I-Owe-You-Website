386668848eb31e48f16cd229c0e454de98fa5e9f
yuliuftd <liuyuftd@gmail.com>
2020-10-29 14:15:54 +1100

    true code style change hot key

--- aip/src/components/favourList.js
+++ aip/src/components/favourList.js
@@ -1,64 +1,65 @@
-import React,{useEffect} from 'react';
-import {connect} from 'react-redux';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import FavourContent from './favourContent';
-import {getFavoursLoading,getValidFavours,getInvalidFavours} from '../Redux/selectors';
-import {LoadFavours,
-        RemoveFavoursRequest,
-        AcceptFavourRequest,
-        AddCommentRequest,
-        AddAwardRequest,
-        } from '../Redux/thunks';
+import { getFavoursLoading, getValidFavours, getInvalidFavours } from '../Redux/selectors';
+import {
+    LoadFavours,
+    RemoveFavoursRequest,
+    AcceptFavourRequest,
+    AddCommentRequest,
+    AddAwardRequest,
+} from '../Redux/thunks';
 // ----------------------------------------------------------------------------------------------------------------------
 
-const FavourList = ({page, size, options,
-                    validFavours,
-                    invalidFavours,
-                    isLoading,
-                    onRemovePressed,
-                    onAcceptPressed,
-                    onDisplayAlertClicker,
-                    startLoadingFavours,
-                    onAddCommentPressed,
-                    onAddAwardPressed,
-                    index})=>{
-                                useEffect(()=>{
-                                    startLoadingFavours(page, size, options);      
-                                },[startLoadingFavours, page, size] );
-                                const LoadingMessage = <div>Is loading...</div>
-                                return isLoading?LoadingMessage:index==='home'?
-                                        <FavourContent 
-                                            validFavours={validFavours} 
-                                            onRemovePressed={onRemovePressed} 
-                                            onAcceptPressed={onAcceptPressed} 
-                                            onDisplayAlertClicker={onDisplayAlertClicker} 
-                                            onAddAwardPressed={onAddAwardPressed} 
-                                            onAddCommentPressed={onAddCommentPressed}
-                                            index={index}/>
-                                            :index==='mypage'?<FavourContent
-                                                                validFavours={validFavours} 
-                                                                invalidFavours={invalidFavours}
-                                                                onRemovePressed={onRemovePressed} 
-                                                                onAcceptPressed={onAcceptPressed} 
-                                                                onDisplayAlertClicker={onDisplayAlertClicker} 
-                                                                onAddAwardPressed={onAddAwardPressed} 
-                                                                onAddCommentPressed={onAddCommentPressed}
-                                                                index={index}
-                                                                />:null
-                    }; 
+const FavourList = ({ page, size, options,
+    validFavours,
+    invalidFavours,
+    isLoading,
+    onRemovePressed,
+    onAcceptPressed,
+    onDisplayAlertClicker,
+    startLoadingFavours,
+    onAddCommentPressed,
+    onAddAwardPressed,
+    index }) => {
+    useEffect(() => {
+        startLoadingFavours(page, size, options);
+    }, [startLoadingFavours, page, size]);
+    const LoadingMessage = <div>Is loading...</div>
+    return isLoading ? LoadingMessage : index === 'home' ?
+        <FavourContent
+            validFavours={validFavours}
+            onRemovePressed={onRemovePressed}
+            onAcceptPressed={onAcceptPressed}
+            onDisplayAlertClicker={onDisplayAlertClicker}
+            onAddAwardPressed={onAddAwardPressed}
+            onAddCommentPressed={onAddCommentPressed}
+            index={index} />
+        : index === 'mypage' ? <FavourContent
+            validFavours={validFavours}
+            invalidFavours={invalidFavours}
+            onRemovePressed={onRemovePressed}
+            onAcceptPressed={onAcceptPressed}
+            onDisplayAlertClicker={onDisplayAlertClicker}
+            onAddAwardPressed={onAddAwardPressed}
+            onAddCommentPressed={onAddCommentPressed}
+            index={index}
+        /> : null
+};
 
-const mapStateToProps = state =>({
+const mapStateToProps = state => ({
     isLoading: getFavoursLoading(state),
-    validFavours:getValidFavours(state),
-    invalidFavours:getInvalidFavours(state),
+    validFavours: getValidFavours(state),
+    invalidFavours: getInvalidFavours(state),
 });
-const mapDispatchToProps = dispatch =>({
-    startLoadingFavours:(page, size, options)=>dispatch(LoadFavours(page, size, options)),
-    onRemovePressed: favour=>dispatch(RemoveFavoursRequest(favour)),
-    onAcceptPressed: favour=>dispatch(AcceptFavourRequest(favour)),
-    onAddCommentPressed: comment=>dispatch(AddCommentRequest(comment)),
-    onAddAwardPressed: award=>dispatch(AddAwardRequest(award))
+const mapDispatchToProps = dispatch => ({
+    startLoadingFavours: (page, size, options) => dispatch(LoadFavours(page, size, options)),
+    onRemovePressed: favour => dispatch(RemoveFavoursRequest(favour)),
+    onAcceptPressed: favour => dispatch(AcceptFavourRequest(favour)),
+    onAddCommentPressed: comment => dispatch(AddCommentRequest(comment)),
+    onAddAwardPressed: award => dispatch(AddAwardRequest(award))
 })
 
 
 
-export default connect(mapStateToProps,mapDispatchToProps)(FavourList);
+export default connect(mapStateToProps, mapDispatchToProps)(FavourList);
